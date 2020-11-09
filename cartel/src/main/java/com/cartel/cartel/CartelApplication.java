0c9fcbb19a9663396eb68a3578f0f51a9b5ae677
MatthiasChometon1 <matthias.chometon@ynov.com>
2020-11-09 16:03:30 +0100

    initialize project

---
+++ cartel/src/main/java/com/cartel/cartel/CartelApplication.java
@@ -0,0 +1,13 @@
+package com.cartel.cartel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartelApplication.class, args);
+	}
+
+}
