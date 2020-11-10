00fcc8bcfcf510ee7381527b7e019d6f23286958
MatthiasChometon1 <matthias.chometon@ynov.com>
2020-11-10 18:25:21 +0100

    update CardOrder

--- cartel/src/main/java/com/cartel/cartel/Model/cardorder.java
+++ cartel/src/main/java/com/cartel/cartel/Model/cardorder.java
@@ -1,48 +1,103 @@
 package com.cartel.cartel.Model;
 
 import java.sql.Date;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.JoinColumn;
 
-public class cardorder {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "T_CardOrder")
+@Entity
+public class CardOrder {
 	
-	private Integer idCardOrder;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
-	private Integer price;
-	
+	private float price;
 	private Date creation;
+	private Integer user;
 	
-	private User user;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+    @JoinTable(
+            name = "product_cardorder",
+            joinColumns = {@JoinColumn(name = "cardorder")},
+            inverseJoinColumns = {@JoinColumn(name = "product")}
+    )
+    private Set<Product> products = new HashSet<>();
 
-	public Integer getIdCardOrder() {
-		return idCardOrder;
-	}
-
-	public void setIdCardOrder(Integer idCardOrder) {
-		this.idCardOrder = idCardOrder;
-	}
-
-	public Integer getPrice() {
+	/**
+	 * @return the price
+	 */
+	public float getPrice() {
 		return price;
 	}
 
-	public void setPrice(Integer price) {
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(float price) {
 		this.price = price;
 	}
 
+	/**
+	 * @return the creation
+	 */
 	public Date getCreation() {
 		return creation;
 	}
 
+	/**
+	 * @param creation the creation to set
+	 */
 	public void setCreation(Date creation) {
 		this.creation = creation;
 	}
 
-	public User getUser() {
+	/**
+	 * @return the user
+	 */
+	public Integer getUser() {
 		return user;
 	}
 
-	public void setUser(User user) {
+	/**
+	 * @param user the user to set
+	 */
+	public void setUser(Integer user) {
 		this.user = user;
 	}
+	
+    public Set<Product> getProducts() {
+        return products;
+    }
+    
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", price='" + price + '\'' +
+                ", ceation='" + creation + '\'' +
+                ", user='" + user + '\'' +
+                ", products='" + products.stream().map(Product::getName).collect(Collectors.toList()) + '\'' +
+                '}';
+    }
 
 }
